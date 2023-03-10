2de68474d2319395d532e452746cbdcf4db15014
Văn Nghĩa <nghiazorovn2304@gmail.com>
2023-03-10 10:55:14 +0700

    f

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,30 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext dbContext;
+        public CoursesController()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+
+
+            var viewModel = new CourseViewModel { Categories = dbContext.Categories.ToList() };
+
+            return View(viewModel);
+
+        }
+    }
+}
